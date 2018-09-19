c2f978b626a4c4175582c4a285df7e54e8210fd6
Angela <acsmoot14@gmail.com>
2018-09-19 16:39:39 -0500

    guessFootballAnswer Modified, added pseudocode for answer logic, added user guess to wbpg

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 //Choose a theme for your game!
-//array u ready for some football questions
+//Array u ready for some football
+// CLASS NOTES CHECK OUT MONDAY SLACK
+// var obj = [ {q: word?, a:answer}, {q1: word?, a1:answer},]o
 var footballQuestions = [
   "How many years must a player be retired to be eligible for the Pro Football Hall of Fame",
   "Who had the most rushing yards in the NFL for the 1980's",
@@ -19,117 +21,159 @@ var footballAnswer = [
   "1985"
 ];
 
-//get the game started
-// start the game
-//give directions to start the game
+// directions to start the game
 var directionsText = document.getElementById("directions-text");
 directionsText.textContent = "Press any key to get started!";
-document.onkeypress = function() {
-  console.log("game has started");
-  directionsText.textContent = "";
-  startGame();
-};
-
 //variables
 var footballQuestion = document.getElementById("footballQuestion-text");
+
 console.log(footballQuestion);
-
-//choose a random football question
-function startGame() {
-  var randomQuestion =
-    footballQuestions[Math.floor(Math.random() * footballQuestions.length)];
-  console.log(randomQuestion);
-  footballQuestion.textContent =
-    "Please answer the following football question: " + randomQuestion;
-  getFootballKey(randomQuestion);
-}
-//////////
-
-//get the index of the random question as the key
-
-//  var key="Real Name";
-
-//  index=joanOfArcInfoParts.indexOf(key);
-//  var info=joanOfArcInfoValues[index];
-//  console.log(key, index, info);
-//
-/////
-function getFootballKey(randomQuestion) {
-  for (index = 0; index < footballQuestions.length; index++) {
-    if (randomQuestion === footballQuestions[index]) {
-      console.log(index);
-      getFootballAnswer(index);
-    } 
-    // else {
-    //   alert("answer was not present in array");
-    // }
-  }
-}
-// then get the answer by looping through football answer with the index matching the random question
-function getFootballAnswer(key) {
- // var maskedAnswer = "";
-  var answer = footballAnswer[key];
-  //alert(answer);
-  console.log(answer);
-  maskFootballAnswer(answer);
-}
-
-function maskFootballAnswer(answer){
- //convert answer into a character array
- // assigned maskedanswer    to characters
- //Display answer as `**********` or `????????`
- //    If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`.
-var maskedAnswer="";
-var arrayAnswer = [];
-arrayAnswer = answer.toCharArray();
-console.log(arrayAnswer);
-
-
-// using the answer as an array
-// have the array.length be Total number of guesses
-// each guess is logged
-//Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`).
-// /var guessedChars = "";
-// if guess is wrong
-    //guesscounter is decremented unti guesses is 0 then increment loss
-
-//    As the user guesses the correct letters, reveal them: `m a d o _  _ a`    
-//if guess is right
-    // guesscounter not decremented
-    // loop through answer and replace maskchar for char in arrayAnswer
-
-
-//no more maskchar then increment wins
-
-
-}
+//Game Display Stats
+var winsText = document.getElementById("wins-text");
+var loseText = document.getElementById("losses-text");
+var guessCountText = document.getElementById("guess-remain");
+//Inital Game Stats
 //Display initall table with zeros
 // (# of times user guessed word correctly).
 var wins = 0;
-var winsText = document.getElementById("wins-text");
-winsText.textContent = +wins;
-
-// (# of times user has lost the game).
 var losses = 0;
-var loseText = document.getElementById("losses-text");
-loseText.textContent = +losses;
+var guessCount = 0;
 
+winsText.textContent = +wins;
+// (# of times user has lost the game).
+loseText.textContent = +losses;
 //Number of Guesses Remaining: (# of guesses remaining for the user).
 //Decrement from the total number of letters in the answer
 //guesscount will initalize to the total number of characters in the answer
-var guessCount = 0;
-var guessCountText = document.getElementById("guess-remain");
 guessCountText.textContent = +guessCount;
 
+//get the game started
+document.onkeyup = function(event) {
+  var userGuess = event.key;
+  if (event.key === "Escape") {
+    wins = 0;
+    losses = 0;
+    ties = 0;
+  } else {
+    console.log("game has started");
+    startGame();
+  }
+};
 
-//select the element from the DOM
+function startGame() {
+  //choose a random football question
+  var randomQuestion =
+    footballQuestions[Math.floor(Math.random() * footballQuestions.length)];
 
-//sat class notes
-//using doucment
-// var asterSpan=document.getElementById("asterisks");
-// console.log(asterSpan);
-// var asterword="";
-//for loop the answer.length
-//add it to the emptyword;
-///asterspan.innertext = asterword;
-//console.log(asterword)
+  footballQuestion.textContent =
+    "Answer the following football question: " + randomQuestion;
+
+  console.log(randomQuestion);
+
+  // Hide the directions
+  directionsText.textContent = "";
+  getFootballAnswer(randomQuestion);
+}
+
+//  get answer by looping through football answer with index matching random question
+function getFootballAnswer(randomQuestion) {
+  for (index = 0; index < footballQuestions.length; index++) {
+    if (randomQuestion === footballQuestions[index]) {
+      // extra TBD      console.log(index);
+      var answer = footballAnswer[index];
+      console.log(answer);
+    }
+  }
+  maskFootballAnswer(answer);
+}
+
+function maskFootballAnswer(answer) {
+  //   // assigned answer to masked characters
+  //   //    If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`.
+  var maskedAnswer = "";
+  var maskedCharacter = "?";
+  for (i = 0; i < answer.length; i++) {
+    maskedAnswer = maskedAnswer.replace(
+      maskedAnswer.charAt(i),
+      maskedCharacter
+    );
+  }
+  console.log(maskedAnswer);
+  //Display football answer as `**********` or `????????`
+  var maskedAnswerText = document.getElementById("footballAnswer-text");
+  maskedAnswerText.textContent = "Guess: " + maskedAnswer;
+  guessFootballAnswer(answer, maskedAnswer);
+}
+
+function guessFootballAnswer(answer, maskedAnswer) {
+  // var missing = 0;
+  // var wrongGuess = true;
+
+  var userGuess = "";
+  var lettersguessed = []; // each guess is logged
+  var answerLength = answer.length; // have the array.length be Total number of guesses
+  // var totalGuesses = answer.length; // have the array.length be Total number of guesses
+  // var replacedWord = maskedAnswer;
+  var letterGuessedText = document.getElementById("footballGuess-text");
+
+  // while (answerLength > 0) {
+  document.onkeyup = function(event) {
+    // for (var j = 0; j < answer.Length; j++) {
+    // Determines which key was pressed.
+    userGuess = event.key;
+    wrongGuess = false;
+
+    // lettersguessed = lettersguessed.push(userGuess);
+    console.log("They guessed " + userGuess);
+    // console.log("user has entered" + j + "characters");
+    letterGuessedText.textContent = "Your guess is " + userGuess;
+
+    for (k = 0; k < answer.length; k++) {
+      if (answer.charAt(k) == userGuess) {
+        //replace the char in maskedAns with guess
+        console.log("guess is in the answer " + answer.charAt(k));
+        //replace ? with char
+        maskedAnswer = maskedAnswer.replace(maskedAnswer.charAt(k), userGuess);
+        console.log(maskedAnswer);
+        // maskedAnswer2 = maskedAnswer.setCharAt(k, userGuess);
+        // console.log(maskedAnswer2);
+        wrongGuess = true;
+        console.log("answerLength " + answerLength);
+        answerLength--;
+        // console.log(answer.length + " was changed");
+        //display
+      } else if (userGuess == maskedAnswer.charAt(k)) {
+        console.log("Already guessed this letter");
+      } else if (wrongGuess) {
+        console.log("NOT IN THE ANSWER");
+        //counter
+        // if guess is wrong
+        //guesscounter is incremented
+        if (answerLength == 0) {
+          //  increment loss and game over
+          console.log("GAME OVER");
+        }
+      }
+    }
+  };
+  // }
+  //Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`).
+  // lettersguessed = lettersguessed.push(userGuess);
+
+  console.log("in the guessFootballAnswer function");
+  console.log("The answer Length is " + answerLength);
+  for (var k = 0; k < lettersguessed.length; k++) {
+    console.log("in the array" + letterguessed[k]);
+    console.log("Letters the user has guessed " + lettersguessed[k]);
+  }
+
+  // if guess is wrong
+  //guesscounter is decremented unti guesses is 0 then increment loss
+
+  // As the user guesses the correct letters, reveal them: `m a d o _  _ a`
+  //if guess is right
+  // guesscounter not decremented
+  // loop through answer and replace maskchar for char in arrayAnswer
+
+  //no more maskchar then increment wins
+}
